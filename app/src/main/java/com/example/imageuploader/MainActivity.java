107db2643504bc4091146a56530b44dc8d074a95
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-01 21:21:09 -0400

    Added image picker functionality

--- app/src/main/java/com/example/imageuploader/MainActivity.java
+++ app/src/main/java/com/example/imageuploader/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.imageuploader;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICk_IMAGE_REQUEST = 1;
+    private Button mButtonChooseImage, mButtonUploadImage;
+    private TextView mTextViewShowUploads;
+    private ProgressBar mProgressBar;
+    private ImageView mImageView;
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonChooseImage = findViewById(R.id.choose_image);
+        mImageView = findViewById(R.id.image_view);
+
+
+
+        mButtonChooseImage.setOnClickListener(view -> openImageChooser());
+    }
+
+    public void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICk_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICk_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+
+            Picasso.with(this).load(mImageUri).into(mImageView);
+        }
     }
 }
